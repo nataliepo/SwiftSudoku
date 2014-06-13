e2e47b973dd73cf52ce09cb5a2f5a38a0a090d9f
nataliepo <natalie@29.io>
2014-06-12 23:38:09 -0400

    starting out my first swift project (doesnt do much yet)

---
+++ SwiftAlpha/BoardController.swift
@@ -0,0 +1,84 @@
+//
+//  BoardController.swift
+//  SwiftAlpha
+//
+//  Created by natalie on 6/12/14.
+//  Copyright (c) 2014 Natalie Podrazik. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class BoardController: UIViewController  {
+
+    var cellArray: Array<UIButton> = []
+
+    struct constants {
+        static let BOARD_EDGE = Float(3.0)
+        static let MAX_CELLS = pow(3.0, 4.0)
+        static let EDGE_SIZE_W = Float(35.0)
+        static let EDGE_SIZE_H = Float(38.0)
+    }
+    
+    func buttonTapped(button_tapped: UIButton) {
+        println("you tapped a button, tag = \(button_tapped.tag)")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.setUpCellGrid()
+    }
+    
+    func setUpCellGrid() {
+        let buffer = 2.0
+        
+        for i in 0..(constants.BOARD_EDGE * constants.BOARD_EDGE) {
+            for j in 0..(constants.BOARD_EDGE * constants.BOARD_EDGE) {
+                
+                var x_buffer = 0, y_buffer = 0
+                
+                var frame = CGRect(x:(Float(j) * constants.EDGE_SIZE_W) + Float(x_buffer),
+                    y:(Float(i) * constants.EDGE_SIZE_H) + Float(buffer),
+                    width:constants.EDGE_SIZE_W,
+                    height:constants.EDGE_SIZE_H);
+                
+                var index = (i * (constants.BOARD_EDGE * constants.BOARD_EDGE)) + j
+                
+                let button = UIButton()
+                if ((j % 2) == (i % 2)) {
+                    button.backgroundColor = UIColor.clearColor()
+                }
+                else {
+                    button.backgroundColor = UIColor.whiteColor()
+                }
+                
+                button.addTarget(self,
+                                 action:Selector("buttonTapped:"),
+                    forControlEvents: UIControlEvents.TouchUpInside)
+
+                
+                button.frame = frame
+                button.tag = ((Int(i + 1) * 10) + Int(j + 1))
+                
+                
+                cellArray.insert(button, atIndex:Int(index))
+                
+                self.view.addSubview(button)
+            }
+        }
+        
+        var width = constants.EDGE_SIZE_W * constants.BOARD_EDGE * constants.BOARD_EDGE
+        var height = constants.EDGE_SIZE_H * constants.BOARD_EDGE * constants.BOARD_EDGE
+        
+        // finally, resize self view to be exactly as big as the board.
+        self.view.frame.size = CGSizeMake(width, height)
+    }
+    
+
+}
---
+++ SwiftAlpha/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  SwiftAlpha
+//
+//  Created by natalie on 6/11/14.
+//  Copyright (c) 2014 Natalie Podrazik. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    // UX elements
+    var boardViewController = BoardController()
+    
+    init(coder aDecoder: NSCoder!)  {
+        super.init(coder: aDecoder)
+
+        // self.boardView = BoardController()
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // center frame.
+        var y_offset = (self.view.frame.height - boardViewController.view.frame.height) / 2
+        var x_offset = (self.view.frame.width - boardViewController.view.frame.width) / 2
+        
+        boardViewController.view.frame = CGRectMake(x_offset,
+                                                    y_offset,
+                                                    boardViewController.view.frame.width,
+                                                    boardViewController.view.frame.width)
+        
+        
+        println("frame: \(NSStringFromCGRect(boardViewController.view.frame))")
+        
+        self.view.addSubview(boardViewController.view)
+        
+    }
+    
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
+
